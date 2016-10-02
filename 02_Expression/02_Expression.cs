6bae91ef1f92ef6900089b1b2c5acb84731eef50
Slavyanaa <Slavyana_ch@yahoo.com>
2016-10-02 17:02:14 +0300

    Changed expressions (*2)

--- 02_Expression/02_Expression.cs
+++ 02_Expression/02_Expression.cs
@@ -10,7 +10,7 @@ namespace _02_Expression
     {
         static void Main(string[] args)
         {
-            Console.WriteLine((3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3);
+            Console.WriteLine((3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 2);
         }
     }
 }
